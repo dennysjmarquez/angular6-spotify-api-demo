e3e7c9df20192d3391edcf571b8a27be5a16fcba
Dennys J Marquez <dennysjmarquez@gmail.com>
2019-06-15 20:30:04 -0400

    no message

--- src/app/components/services/spotify.service.ts
+++ src/app/components/services/spotify.service.ts
@@ -19,7 +19,7 @@ export class SpotifyService {
 
     authorize: 'https://accounts.spotify.com/es-ES/authorize?client_id=' +
       this.credentials.clientId + '&response_type=token' +
-      '&redirect_uri=' + encodeURIComponent('/#/') +
+      '&redirect_uri=' + encodeURIComponent('https://dennysjmarquez.github.io/angular7-spotify/dist/spotify/') +
       '&expires_in=3600',
     refreshaAcessToken: 'https://accounts.spotify.com/api/token'
 
